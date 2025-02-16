3027072311efd68e84fc34a53b9527f121ffd158
Aufa Azzahidi <aufa.azzahidi@gmail.com>
2025-02-16 20:08:35 +0700

    finished creating API'

--- main.go
+++ main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+    "golang-crud-users/config"
+    "golang-crud-users/models"
+    "golang-crud-users/routes"
+)
+
+func main() {
+    config.ConnectDB()
+    models.Migrate(config.DB)
+
+    r := routes.SetupRouter()
+    r.Run(":8080")
+}
